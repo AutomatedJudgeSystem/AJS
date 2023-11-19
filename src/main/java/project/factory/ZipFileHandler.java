c05f525e070aea65b010adf927a73910d7364717
SGajadhar <97929776+SGajadhar@users.noreply.github.com>
2023-11-19 05:57:56 -0400

    updated zipFileHandler

--- src/main/java/project/factory/ZipFileHandler.java
+++ src/main/java/project/factory/ZipFileHandler.java
@@ -55,8 +55,47 @@ public class ZipFileHandler implements FileHandler{
         }            
     }
 
-    private void processExtractedFiles(String extractionPath){
-        //add logic for processing
-        System.out.println("Processing..");
+    private void processExtractedFiles(String extractionPath) throws IOException {
+        //get all the files in extractedFiles
+        try(DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(extractionPath))){
+            for (Path filePath : directoryStream){
+                //Check if file is a zip file
+                if(Files.isRegularFile(filePath) && filePath.toString().endsWith(".zip")) {
+                    //Recursively handle the nested zip files
+                    handleNestedZipFile(filePath.toString(), extractionPath);
+                }
+                else if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".java")) {
+                    //Process Java Files
+                    System.out.println("Processing Java file: " + filePath.getFileName());
+                }
+            }
+        }
+
+        //Add other logic for process here
+    }
+
+    private void handleNestedZipFile(String nestedZipFilePath, String extractionPath) throws IOException {
+        //Create a subdirectoru for the nested zip file
+        String nestedZipDirectoryName = getFileNameWithoutExtension(nestedZipFilePath);
+        String nestedZipDirectoryPath = Paths.get(extractionPath, nestedZipDirectoryName).toString();
+        createDirectory(nestedZipDirectoryPath);
+
+        //Extract the contents of the nested zip file into the subdirectory 
+        extractZipFile(nestedZipFilePath, nestedZipDirectoryPath);
+        
+        //Process the extracted the files from the nested zip file
+        processExtractedFiles(nestedZipDirectoryPath);
+    }
+
+    private String getFileNameWithoutExtension(String filePath){
+        String fileName = Paths.get(filePath).getFileName().toString();
+        int lastDotIndex = fileName.lastIndexOf(".");
+
+        if(lastDotIndex != -1){
+            return fileName.substring(0, lastDotIndex);
+        }
+        else {
+            return fileName;
+        }
     }
 }
